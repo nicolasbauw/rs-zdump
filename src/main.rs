5330b77cb2af8a80ca26bef6936e24b563be7b0f
Nicolas BAUW <nicolas.bauw@leroymerlin.fr>
2019-11-05 14:24:49 +0100

    Revert "Year defaults to current year"
    This reverts commit 1288820397e1ab33cd8070d7ca7b17f15292fe51.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate tzparse;
 use structopt::StructOpt;
-use chrono::prelude::*;
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "My Rust version of zdump")]
@@ -21,24 +20,21 @@ fn main() {
     // Parsing timezone's TZfile
     let timechanges = match tzparse::get(&opt.timezone, opt.year) {
         Some(tc) => tc,
-        None => { println!("No data for requested timezone"); return }
+        None => return
     };
     let tzdata = match tzparse::worldtime(&timechanges) {
         Some(tz) => tz,
         None => return
     };
 
-    let year: i32 = match opt.year {
-        Some(y) => y,
-        None => {
-            let d = Utc::now();
-            d.format("%Y").to_string().parse().unwrap()
-                }
-    };
-    
-    println!("{} {} {}, week number: {}\n", &opt.timezone, tzdata.datetime.to_rfc2822(), tzdata.abbreviation, tzdata.datetime.format("%W").to_string());
-    println!("Timechanges (or last timechange) for {} in {} (UTC):", &opt.timezone, year);
-    for i in &timechanges {
-        println!("{}", i.time.format("%a %e %b %T %Y").to_string());
+    match opt.year {
+        None => println!("{} {} {}, week number: {}", &opt.timezone, tzdata.datetime.to_rfc2822(), tzdata.abbreviation, tzdata.datetime.format("%W").to_string()),
+        Some(y) => {
+            println!("Timechanges for {} in {} (UTC):", &opt.timezone, y);
+            for i in &timechanges {
+                println!("{}", i.time.format("%a %e %b %T %Y").to_string());
+            }
+        }
     }
 }
+
