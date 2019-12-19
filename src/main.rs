3825c77099016333cd71d47c32ae55b2711bb19d
Nicolas BAUW <nbauw@hotmail.com>
2019-12-19 14:16:07 +0100

    Added crate documentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+//! An alternative version of the zdump utility.
+//!
+//! Without arguments, shows actual time and data about the Europe/Paris zone:
+//! ```text
+//! zdump
+//! Europe/Paris Thu, 19 Dec 2019 13:48:47 +0100 CET, week number: 51
+//! ````
+//! 
+//! Same thing when specifying a zonename: 
+//! ```text
+//! zdump America/Phoenix
+//! America/Phoenix Thu, 19 Dec 2019 05:52:04 -0700 MST, week number: 51
+//! ````
+//!
+//! When specifying a zonename and a year, outputs timechanges for that year: 
+//! ```text
+//! zdump Europe/Paris -y 2020
+//! Europe/Paris Sun 29 Mar 01:00:00 2020 UT -> CEST gmtoff=7200 DST: true
+//! Europe/Paris Sun 25 Oct 01:00:00 2020 UT -> CET gmtoff=3600 DST: false 
+//! ```
+
 extern crate tzparse;
 use structopt::StructOpt;
 
