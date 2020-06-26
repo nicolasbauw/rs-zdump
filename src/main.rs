82270c6f16fdc4cb67b5d9c9845b4b1ab36ad5ce
Nicolas BAUW <nbauw@hotmail.com>
2020-06-26 07:13:45 +0200

    Description update

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,20 @@
 //! 
 //! Sample output when specifying only a zonename: 
 //! ```text
-//! zdump America/Phoenix
+//! zdump /usr/share/zoneinfo/America/Phoenix
 //! America/Phoenix Thu, 19 Dec 2019 05:52:04 -0700 MST, week number: 51
 //! ````
 //!
 //! When specifying a zonename and a year, outputs timechanges for that year: 
 //! ```text
-//! zdump Europe/Paris -y 2020
+//! zdump /usr/share/zoneinfo/Europe/Paris -y 2020
 //! Europe/Paris Sun 29 Mar 01:00:00 2020 UT -> CEST gmtoff=7200 DST: true
 //! Europe/Paris Sun 25 Oct 01:00:00 2020 UT -> CET gmtoff=3600 DST: false 
 //! ```
 //! 
 //! To display all zone's transition times: 
 //! ```text
-//! zdump America/Phoenix -a     
+//! zdump /usr/share/zoneinfo/America/Phoenix -a     
 //! America/Phoenix Sun 18 Nov 19:00:00 1883 UT -> MST gmtoff=-25200 DST: false
 //! America/Phoenix Sun 31 Mar 09:00:00 1918 UT -> MDT gmtoff=-21600 DST: true
 //! America/Phoenix Sun 27 Oct 08:00:00 1918 UT -> MST gmtoff=-25200 DST: false
